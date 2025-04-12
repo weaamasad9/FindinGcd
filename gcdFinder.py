4601259b2b43ae4b65d3c243a36ca92bda50d0f1
weaam <weaamasad9@gmail.com>
2025-04-12 14:40:21 +0300

    Add files via upload

---
+++ gcdFinder.py
@@ -0,0 +1,17 @@
+"""
+Dear students, for your convenience, attached strings to be used.
+Notice that there is a difference in the strings if there is only one element in a list or more than one element.
+Having '_' in a string meaning that a relevant integer number has to be determined in a run time.
+"""
+#Enter number of lists, please:
+#Enter length of list no. _, please: 
+#Enter 1 integer, please: 
+#Enter _ integers, please: 
+#List of lists:
+#Has at least one pair of coprime numbers in symmetric locations.
+#Has no pair of coprime numbers in symmetric locations.
+#Thank you for exploring GCD and list of lists using recursion.
+"""
+Add doccumentation in the code and near every function explain in details (including meaning) what are the paramerers it gets ,
+what it does, and what does it return.
+"""
